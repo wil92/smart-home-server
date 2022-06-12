962eadd5881325c3c297e0e3f987212deb286ce1
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2022-06-12 23:00:47 +0200

    Add SH prefix to the env vars

--- src/environments.js
+++ src/environments.js
@@ -1,11 +1,11 @@
 const env = {
-  key: process.env.KEY || 'UQL7VpL51OAVvtZQk2Xd',
-  username: process.env.USERNAME || 'admin',
-  password: process.env.PASSWORD || 'admin124!',
-  auth2ClientId: process.env.AUTH2_CLIENT_ID || '5IRJ5rSNSqqqPT2',
-  auth2ClientSecret: process.env.AUTH2_CLIENT_SECRET || '6RP64F4sHkZ7FTcCQoZZ6S5Ot',
-  auth2redirectUri: process.env.AUTH2_REDIRECT_URI || 'REDIRECT_URI',
-  googleUserId: process.env.GOOGLE_USER_ID || 'cfrombBECh1bi9N',
+  key: process.env.SH_KEY || 'UQL7VpL51OAVvtZQk2Xd',
+  username: process.env.SH_USERNAME || 'admin',
+  password: process.env.SH_PASSWORD || 'admin124!',
+  auth2ClientId: process.env.SH_AUTH2_CLIENT_ID || '5IRJ5rSNSqqqPT2',
+  auth2ClientSecret: process.env.SH_AUTH2_CLIENT_SECRET || '6RP64F4sHkZ7FTcCQoZZ6S5Ot',
+  auth2redirectUri: process.env.SH_AUTH2_REDIRECT_URI || 'REDIRECT_URI',
+  googleUserId: process.env.SH_GOOGLE_USER_ID || 'cfrombBECh1bi9N',
 };
 
 module.exports = env;
