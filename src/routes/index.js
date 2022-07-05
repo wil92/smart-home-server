d4d34de8fce2c27934e40b97d9d26f4bb20bac06
Guillermo <ggjnez92@gmail.com>
2022-07-05 23:42:09 +0200

    Add online/offline attribute to the list of devices

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
+
 const isLogin = require("../middlewares/is-login");
 const {models} = require("../models");
+const ws = require('../socket/web-socket');
 
 router.use(isLogin);
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  const devices = await models.Device.find({});
-  console.log(devices.map(d => d.toJSON()))
-  res.render('index', {
-    devices: devices.map(d => d.toJSON())
+  let devices = await models.Device.find({});
+  devices = devices.map(d => {
+    return {
+      ...d.toJSON(),
+      online: ws.connectedDevices.has(d.did)
+    };
   });
+  res.render('index', {devices});
 });
 
 router.get('/policy', function (req, res, next) {
